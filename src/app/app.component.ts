9d3c9eecc955a137e66625900a9f38dd9b423262
Rogerio Bautz <rogeriobautz@gmail.com>
2025-05-13 16:55:41 -0300

    feat: aulas 01 e 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { LivroComponent } from './componentes/livro/livro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, LivroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
